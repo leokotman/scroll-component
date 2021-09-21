9ead4f7f400997709bab100da24dd3569d400760
levkotman <leoktmn@yandex.ru>
2021-09-21 16:34:53 +0200

    added scroller & first data

--- src/components/TheScroller.vue
+++ src/components/TheScroller.vue
@@ -1,19 +1,31 @@
 <template>
-    <div>
-
-    </div>
+  <section class="scroller">
+    <li v-for="item in list" :key="item">
+      {{ item }}
+    </li>
+  </section>
 </template>
 
 <script>
 export default {
-    setup () {
-        
-
-        return {}
-    }
-}
+  props: ["list"],
+};
 </script>
 
 <style scoped>
-
+.scroller {
+  background-image: url("../assets/bg-iphone.png");
+  background-repeat: no-repeat;
+  background-size: auto;
+  font-family: Open Sans;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: white;
+  width: 276px;
+  height: 460px;
+}
 </style>
